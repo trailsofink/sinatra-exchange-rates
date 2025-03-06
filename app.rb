1afe225656aa4523ff4d363ea55967c85d6237f2
Joel Hunt <92796238+trailsofink@users.noreply.github.com>
2025-03-06 02:22:12 +0000

    make exchange_api instance variable

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ Dotenv.load
 
 def call_api()
   # EXCHANGE LIST
-  exchange_api = ENV.fetch("EXCHANGE_RATE_KEY")
-  currencies = HTTP.get("https://api.exchangerate.host/list?access_key=#{exchange_api}")
+  @exchange_api = ENV.fetch("EXCHANGE_RATE_KEY")
+  currencies = HTTP.get("https://api.exchangerate.host/list?access_key=#{@exchange_api}")
   currencies_parsed = currencies.parse
   currencies_hash = currencies_parsed["currencies"]
   @currencies_array = currencies_hash.keys
@@ -35,7 +35,7 @@ get("/:currency_from/:currency_to") do
   @currency_from = params.fetch("currency_from")
   @currency_to = params.fetch("currency_to")
   # EXCHANGE CONVERT
-  currency_convert = HTTP.get("https://api.exchangerate.host/convert?from=#{@currency_from}&to=#{@currency_to}&amount=1&access_key=#{exchange_api}")
+  currency_convert = HTTP.get("https://api.exchangerate.host/convert?from=#{@currency_from}&to=#{@currency_to}&amount=1&access_key=#{@exchange_api}")
   currency_convert_hash = currency_convert.parse
   @currency_convert_result = currency_convert_hash["result"].to_s
   erb(:currency_converted)
